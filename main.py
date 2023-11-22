125e0fb6d91b08bd232cac4232dc96efa0ad66f7
Bene <holabenedict@gmail.com>
2023-11-22 19:33:54 +0100

    Rename cli arguments

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def main():
     parser = argparse.ArgumentParser(description="Anonymize DICOM study")
 
     # Define command-line arguments
-    parser.add_argument("zip_file_path", help="Path to the input DICOM study ZIP file")
-    parser.add_argument("output_dir", help="Path to the output directory for anonymized DICOM files")
+    parser.add_argument("input_path", help="Path to the input DICOM files")
+    parser.add_argument("output_path", help="Path to the output directory for anonymized DICOM files")
 
     # Parse command-line arguments
     args = parser.parse_args()
 
     # Anonymize DICOM study
-    anonymize_dicom_study(args.zip_file_path, args.output_dir)
+    anonymize_dicom_study(args.input_path, args.output_path)
 
 
 if __name__ == "__main__":
